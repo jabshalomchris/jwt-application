90668c7947134d1a68df5c4da6e9ff6b27cc9ade
Abshalom Christopher <jabshalomchris@gmail.com>
2024-09-23 20:09:09 +0530

    Implemented Jwt token generator and subsets

---
+++ JWTApplication/JWTHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JWTApplication
+{
+    public static class JWTHelper
+    {
+        public static string GenerateToken(Header header, Payload payload, string secretKey)
+        {
+            var payloadSer = JsonConvert.SerializeObject(payload);
+            var headerSer = JsonConvert.SerializeObject(header);
+
+            var headerBase64 = Base64UrlEncode(headerSer);
+            var payloadBase64 = Base64UrlEncode(payloadSer);
+
+            var signatureBase64 = GenerateSignature(headerBase64, payloadBase64, secretKey);
+
+            return $"{headerBase64}.{payloadBase64}.{signatureBase64}";
+        }
+
+        public static string Base64UrlEncode(string stringToDecode)
+        {
+            var bytes = Encoding.UTF8.GetBytes(stringToDecode);
+
+            return Base64UrlEncode(bytes);
+        }
+
+        public static string Base64UrlEncode(byte[] bytes)
+        {
+            var base64 = Convert.ToBase64String(bytes);
+
+            var baseURL = base64.TrimEnd('=').Replace('+', '-').Replace('/', '_');
+            return baseURL;
+        }
+
+        public static string GenerateSignature(string headerBase64, string payloadBase64, string secretKey)
+        {
+            var cipherText = $"{headerBase64}.{payloadBase64}";
+            HMACSHA256 hMACSHA256 = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey));
+            var hashResult = hMACSHA256.ComputeHash(Encoding.UTF8.GetBytes(cipherText));
+
+            return Base64UrlEncode(hashResult);
+
+        }
+    }
+}
--- JWTApplication/Program.cs
+++ JWTApplication/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JWTApplication;
+
+var header = new Header() { Alg= "HS256", Type ="JWT" };
+
+var claims = new Dictionary<string, string>();
+claims.Add("isAdmin", "false");
+claims.Add("sub", "User Login");
+
+var payload = new Payload()
+{
+    Claims = claims
+};
+
+string jwtToken = JWTHelper.GenerateToken(header, payload, "abc123");
